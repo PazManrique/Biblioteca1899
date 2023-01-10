a483e6bf91fa6e59dcfc03924f4c4393dd8a7ac5
María Paz Manrique <maria-paz.manrique@capgemini.com>
2023-01-10 15:31:33 +0100

    first commit

---
+++ src/main/java/com/capgemini/biblioteca1899/Biblioteca1899Application.java
@@ -0,0 +1,13 @@
+package com.capgemini.biblioteca1899;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Biblioteca1899Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Biblioteca1899Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/capgemini/biblioteca1899/Biblioteca1899ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.capgemini.biblioteca1899;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Biblioteca1899ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
