90021b42b4b2bf588536918e805290effee2da7b
María Paz Manrique <maria-paz.manrique@capgemini.com>
2023-01-13 12:18:41 +0100

    background in index

--- src/main/java/com/capgemini/biblioteca1899/service/loan/LoanServiceImpl.java
+++ src/main/java/com/capgemini/biblioteca1899/service/loan/LoanServiceImpl.java
@@ -11,7 +11,6 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 import com.capgemini.biblioteca1899.model.Loan;
 import com.capgemini.biblioteca1899.repository.LoanRepository;
 
